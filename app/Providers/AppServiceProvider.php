a2579ca29c3dbd9d51635b43b11b7abcce033778
Gusti Rahmat <gustirahmat@hotmail.com>
2021-07-14 00:52:24 +0700

    Set booting
    Signed-off-by: Gusti Rahmat <gustirahmat@hotmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,10 +25,28 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      *
+     * @param UrlGenerator $url
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        Schema::defaultStringLength(191);
+
+        if (App::environment(['production', 'staging'])) {
+            $url->forceScheme('https');
+        }
+
+        Carbon::setLocale(config('app.locale'));
+
+        date_default_timezone_set(config('app.timezone'));
+
+        DB::listen(function($sql) {
+            if ($sql->time > 1000) {
+                Log::info('SLOW QUERY DETECTED:');
+                Log::info($sql->sql);
+                Log::info($sql->bindings);
+                Log::info($sql->time);
+            }
+        });
     }
 }
