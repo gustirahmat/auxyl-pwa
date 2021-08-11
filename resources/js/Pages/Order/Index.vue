e46b9b83844655532db55f9fdcbbefc0e31d0cee
finkastwn <winafinkastwn@gmail.com>
2021-08-11 22:58:16 +0700

    coba lagi

--- resources/js/Pages/Order/Index.vue
+++ resources/js/Pages/Order/Index.vue
@@ -2,7 +2,7 @@
   <public-layout>
     <template #header>
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold text-white">Pesanan Saya</h1>
+        <h1 class="text-3xl font-bold text-white">Daftar Pesanan Saya</h1>
       </div>
     </template>
 
